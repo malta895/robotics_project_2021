4cd6b1fde76658740f2d295830d9efea8a14e331
Luca Maltagliati <malta95@hotmail.it>
2021-04-30 13:17:06 +0200

    feat: velocity estimator takes params instead of calculating them

--- src/robotics_hw1/src/velocity_estimator.cpp
+++ src/robotics_hw1/src/velocity_estimator.cpp
@@ -24,16 +24,17 @@
 // timestamps, thus I can use the ExactTime policy
 typedef message_filters::sync_policies::ExactTime<
     robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed,
-    robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, nav_msgs::Odometry>
+    robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed>
     ExactTimePolicy;
 
 typedef message_filters::Synchronizer<ExactTimePolicy> ExactTimeSynchronizer;
 
+// let's take the double value of pi from boost library
 const double pi = boost::math::constants::pi<double>();
 
 class VelocityEstimator {
 private:
-  ros::NodeHandle node_handle;
+  const ros::NodeHandle node_handle;
 
   ros::Publisher publisher;
 
@@ -43,35 +44,35 @@ private:
   message_filters::Subscriber<robotics_hw1::MotorSpeed> subscriber_front_right;
   message_filters::Subscriber<robotics_hw1::MotorSpeed> subscriber_rear_left;
   message_filters::Subscriber<robotics_hw1::MotorSpeed> subscriber_rear_rigt;
-  message_filters::Subscriber<nav_msgs::Odometry> subscriber_scout_odom;
 
   const double initial_pose_x;
   const double initial_pose_y;
   const double initial_pose_theta;
 
   const double real_baseline;
+  const double apparent_baseline;
   const double wheel_radius;
-  const double gear_ratio;//TODO calibrate and use this instead of hardcoded 40
-
-  double get_double_parameter(const std::string &parameter_key);
+  const double gear_ratio;
 
 public:
-  VelocityEstimator();
+  VelocityEstimator(ros::NodeHandle &node_handle, const double &initial_pose_x,
+                    const double &initial_pose_y,
+                    const double &initial_pose_theta,
+                    const double &wheel_radius, const double &real_baseline,
+                    const double &gear_ratio, const double &apparent_baseline);
 
   void sync_and_publish_velocity_message(
       const robotics_hw1::MotorSpeedConstPtr &motor_speed_front_left,
       const robotics_hw1::MotorSpeedConstPtr &motor_speed_front_right,
       const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_left,
-      const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_right,
-      const nav_msgs::OdometryConstPtr &scout_odom);
+      const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_right);
 };
 
 void VelocityEstimator::sync_and_publish_velocity_message(
     const robotics_hw1::MotorSpeedConstPtr &motor_speed_front_left,
     const robotics_hw1::MotorSpeedConstPtr &motor_speed_front_right,
     const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_left,
-    const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_right,
-    const nav_msgs::OdometryConstPtr &scout_odom) {
+    const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_right) {
 
   ROS_INFO("Seq %d, rpm are %f", motor_speed_front_left->header.seq,
            motor_speed_front_left->rpm);
@@ -105,31 +106,69 @@ void VelocityEstimator::sync_and_publish_velocity_message(
   // TODO calculate the speeds below
   //\omega = 2pi * rpm/60
 
-  double omega_l = (2 * pi) * (-motor_speed_front_left->rpm / 60);
-  omega_l /= 40;// TODO calibrator (package?) that from odometry calculates the ratio and the baseline.
-  double omega_r = (2 * pi) * (motor_speed_front_right->rpm / 60);
-  omega_r /= 40;
-  double odom_omega_z = scout_odom->twist.twist.angular.z;
-  double apparent_baseline =
-      (-(omega_l * wheel_radius) + (omega_r * wheel_radius)) / odom_omega_z;
+  // Since often on same timestamps the rpm of the front and back wheels on the
+  // same side differ, maybe because of measurement errors or due the fact that
+  // it is not an ideal robot, we take the mean of the two measurements
 
+  // the left rotation is registered as backwards, so we invert it
+  double rpm_left =
+      -(motor_speed_front_left->rpm + motor_speed_rear_left->rpm) / 2;
+  double rpm_right =
+      (motor_speed_front_right->rpm + motor_speed_rear_right->rpm) / 2;
+
+  double omega_l = (2 * pi) * ((rpm_left * gear_ratio) / 60);
+  double omega_r = (2 * pi) * ((rpm_right * gear_ratio) / 60);
+
+  // from the rotational speeds and the wheel radius we can easily estimate the
+  // linear speed
   velocity_message.twist.linear.x =
       ((omega_l * wheel_radius) + (omega_r * wheel_radius)) / 2;
-  velocity_message.twist.linear.y =
-      apparent_baseline; // FIXME just for debugging, remove this
-  velocity_message.twist.linear.z =
-      apparent_baseline /
-      real_baseline; //\chi FIXME just for debugging, remove this
 
-  velocity_message.twist.angular.x = omega_l;
-  velocity_message.twist.angular.y = omega_r;
-  // velocity_message.twist.angular.z = 0;
+  // we use the formula with the apparent baseline to get omega_z, i.e. the
+  // angular speed of the robot when it turns
+  velocity_message.twist.angular.z =
+      (-(omega_l * wheel_radius) + (omega_r * wheel_radius)) /
+      apparent_baseline;
+  ;
 
   publisher.publish(velocity_message);
 }
 
-double
-VelocityEstimator::get_double_parameter(const std::string &parameter_key) {
+VelocityEstimator::VelocityEstimator(
+    ros::NodeHandle &node_handle, const double &initial_pose_x,
+    const double &initial_pose_y, const double &initial_pose_theta,
+    const double &wheel_radius, const double &real_baseline,
+    const double &gear_ratio, const double &apparent_baseline)
+    : node_handle(node_handle), initial_pose_x(initial_pose_x),
+      initial_pose_y(initial_pose_y), initial_pose_theta(initial_pose_theta),
+      wheel_radius(wheel_radius), real_baseline(real_baseline),
+      gear_ratio(gear_ratio), apparent_baseline(apparent_baseline) {
+
+  // subscribe to the motor speed topics TODO remove hard coding and put
+  // parameters/argvs with topics names
+
+  subscriber_front_right.subscribe(node_handle, "/motor_speed_fr", 100);
+  subscriber_front_left.subscribe(node_handle, "/motor_speed_fl", 100);
+  subscriber_rear_rigt.subscribe(node_handle, "/motor_speed_rr", 100);
+  subscriber_rear_left.subscribe(node_handle, "/motor_speed_rl", 100);
+
+  // publish the topic
+  publisher = node_handle.advertise<geometry_msgs::TwistStamped>(
+      "/robot_twisted_stamped", 10);
+
+  // initialize the time syncronizer
+  time_syncronizer_ptr.reset(new ExactTimeSynchronizer(
+      ExactTimePolicy(100), subscriber_front_left, subscriber_front_right,
+      subscriber_rear_left, subscriber_rear_rigt));
+
+  // register the callback. One placeholder for each function argument
+  time_syncronizer_ptr->registerCallback(
+      boost::bind(&VelocityEstimator::sync_and_publish_velocity_message, this,
+                  _1, _2, _3, _4));
+}
+
+double get_double_parameter(const std::string &parameter_key,
+                            ros::NodeHandle &node_handle) {
   // this is just an helper function used to retrieve the double params from ROS
   // environment. I need this because in C++ I can initialize const in classes
   // only by calling a function in the member initializer list of the
@@ -146,42 +185,6 @@ VelocityEstimator::get_double_parameter(const std::string &parameter_key) {
     return 0;
   }
   return parameter_value;
-}
-
-// the constructor take the parameters and set them to the const
-// variables in the initializer list by calling the helper function defined
-// above
-VelocityEstimator::VelocityEstimator()
-    : initial_pose_x(get_double_parameter("initial_pose_x")),
-      initial_pose_y(get_double_parameter("initial_pose_y")),
-      initial_pose_theta(get_double_parameter("initial_pose_theta")),
-      wheel_radius(get_double_parameter("wheel_radius")),
-      real_baseline(get_double_parameter("initial_pose_theta"))
-
-{
-
-  // subscribe to the motor speed topics TODO remove hard coding and put
-  // parameters/argvs with topics names
-
-  subscriber_front_right.subscribe(node_handle, "/motor_speed_fr", 100);
-  subscriber_front_left.subscribe(node_handle, "/motor_speed_fl", 100);
-  subscriber_rear_rigt.subscribe(node_handle, "/motor_speed_rr", 100);
-  subscriber_rear_left.subscribe(node_handle, "/motor_speed_rl", 100);
-  subscriber_scout_odom.subscribe(node_handle, "/scout_odom", 100);
-
-  // publish the topic
-  publisher = node_handle.advertise<geometry_msgs::TwistStamped>(
-      "/robot_twisted_stamped", 10);
-
-  // initialize the time syncronizer
-  time_syncronizer_ptr.reset(new ExactTimeSynchronizer(
-      ExactTimePolicy(100), subscriber_front_left, subscriber_front_right,
-      subscriber_rear_left, subscriber_rear_rigt, subscriber_scout_odom));
-
-  // register the callback. One placeholder for each function argument
-  time_syncronizer_ptr->registerCallback(
-      boost::bind(&VelocityEstimator::sync_and_publish_velocity_message, this,
-                  _1, _2, _3, _4, _5));
 }
 
 int main(int argc, char **argv) {
@@ -205,7 +208,15 @@ int main(int argc, char **argv) {
   //   return 1;
   // }
 
-  VelocityEstimator odometry_calculator;
+  ros::NodeHandle node_handle;
+  VelocityEstimator odometry_calculator(
+      node_handle, get_double_parameter("initial_pose_x", node_handle),
+      get_double_parameter("initial_pose_y", node_handle),
+      get_double_parameter("initial_pose_theta", node_handle),
+      get_double_parameter("wheel_radius", node_handle),
+      get_double_parameter("real_baseline", node_handle),
+      get_double_parameter("gear_ratio", node_handle),
+      get_double_parameter("apparent_baseline", node_handle));
 
   ros::spin();
 
