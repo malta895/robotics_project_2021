cbfc033b03ef8df1db22a4f3e233bc0bfb4730a5
Luca Maltagliati <malta95@hotmail.it>
2021-05-01 12:49:52 +0200

    feat: parameters calibrator, gear ratio and baseline calibrated

--- src/robotics_hw1/src/velocity_estimator.cpp
+++ src/robotics_hw1/src/velocity_estimator.cpp
@@ -32,7 +32,7 @@ typedef message_filters::Synchronizer<ExactTimePolicy> ExactTimeSynchronizer;
 // let's take the double value of pi from boost library
 const double pi = boost::math::constants::pi<double>();
 
-class VelocityEstimator {
+class eterCalibrator {
 private:
   const ros::NodeHandle node_handle;
 
@@ -55,7 +55,7 @@ private:
   const double gear_ratio;
 
 public:
-  VelocityEstimator(ros::NodeHandle &node_handle, const double &initial_pose_x,
+  ParameterCalibrator(ros::NodeHandle &node_handle, const double &initial_pose_x,
                     const double &initial_pose_y,
                     const double &initial_pose_theta,
                     const double &wheel_radius, const double &real_baseline,
@@ -68,7 +68,7 @@ public:
       const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_right);
 };
 
-void VelocityEstimator::sync_and_publish_velocity_message(
+void ParameterCalibrator::sync_and_publish_velocity_message(
     const robotics_hw1::MotorSpeedConstPtr &motor_speed_front_left,
     const robotics_hw1::MotorSpeedConstPtr &motor_speed_front_right,
     const robotics_hw1::MotorSpeedConstPtr &motor_speed_rear_left,
@@ -133,7 +133,7 @@ void VelocityEstimator::sync_and_publish_velocity_message(
   publisher.publish(velocity_message);
 }
 
-VelocityEstimator::VelocityEstimator(
+ParameterCalibrator::ParameterCalibrator(
     ros::NodeHandle &node_handle, const double &initial_pose_x,
     const double &initial_pose_y, const double &initial_pose_theta,
     const double &wheel_radius, const double &real_baseline,
@@ -162,7 +162,7 @@ VelocityEstimator::VelocityEstimator(
 
   // register the callback. One placeholder for each function argument
   time_syncronizer_ptr->registerCallback(
-      boost::bind(&VelocityEstimator::sync_and_publish_velocity_message, this,
+      boost::bind(&ParameterCalibrator::sync_and_publish_velocity_message, this,
                   _1, _2, _3, _4));
 }
 
@@ -208,7 +208,7 @@ int main(int argc, char **argv) {
   // }
 
   ros::NodeHandle node_handle;
-  VelocityEstimator odometry_calculator(
+  ParameterCalibrator odometry_calculator(
       node_handle, get_double_parameter("initial_pose_x", node_handle),
       get_double_parameter("initial_pose_y", node_handle),
       get_double_parameter("initial_pose_theta", node_handle),
